4afe5d114e785b921b9f04e1dd4b75382bfe4ec4
nolasco7a <nolasco7a@gmail.com>
2019-12-02 09:47:34 -0600

    adicion de tag de google analitycs

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,6 +5,18 @@
         @include('layouts.styles')
     </head>
 <body>
+    {{-- Tag de Google Analitycs --}}
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-150262918-2"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-150262918-2');
+        </script>
+    {{-- /Tag de Google Analitycs --}}
+
     @include('layouts.navbar')
 
     {{-- Contenido Dinamico --}}
