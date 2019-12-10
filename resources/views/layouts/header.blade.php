e5384ed9842e4979728cde1cb1804f691be735d0
nolasco7a <nolasco7a@gmail.com>
2019-12-10 16:52:38 -0600

    add keywords

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,17 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
 <link rel='shortcut icon' type='image/png' href='./assets/images/favicon.png'/>
+<meta name="keywords" content="Navidad Catracha
+Paz, Convivencia, Navidad, Frozen, Santa, Grinch, Tamaleadas, Recreovía, Villa Navideña, Nacimiento, Plazas Navideñas,
+Parque Vida Mejor, Gobierno, Honduras, Catracho, Navidad, Polache, Festival, Frío, Honduras, Compartir,
+Celebrar, Disfrutar, Carrozas, Bandas, Mototaxi, Desfile, Elsa, Niños, Luz, Magia, Color, Celebrar, Compartir,
+Dar, Recibir, Unidad, Prevención, Familias, Valores, Cultura, Tradición, Recreación, Costumbres, Deportes,
+Karaoke, Música, Conciertos, Teatro, Renos, Operación Sticker, Árbol de Navidad, Villa Navideña, Villancicos,
+Pueblo, Tegucigalpa, Ceiba, San Pedro Sula, Trencito, Marcha, Juegos, Santa Claus, Gastronomía, Gorritos,
+Renos, Golosinas, Comidas, Video, Personas, Concursos, Giveaway, Premio, Regalos, Duende, Elfo, Juguetes,
+Diversión, Villa Olímpica, Spot, Navidad 2019, 2019, Año Nuevo, Reyes Magos, Parquecito, Plazita,
+Tamal, Nacatamal, Torreja, Convivio, Cuchumbo, Fin de Año, Nochebuena"/>
 <title>Navidad Catracha</title>
+<meta name="Navidad Catracha, encuentra una gran variedad de actividades que se estaran realizando cerca de tu localidad." />
