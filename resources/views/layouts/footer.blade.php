6508b5eb3a53ff5efe608d064cb814103a0beefc
nolasco7a <nolasco7a@gmail.com>
2019-12-11 20:45:11 -0600

    add link youtube

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,9 +3,10 @@
     <div class="container rs">
         <div class="row">
             <div class="col-lg-6">
-                    <span>Síguenos!<span>
-                            <span class="text-white"><a href="https://www.facebook.com/NavidadCatracha/"><i class="fab fa-facebook"></i></a></span>
-                            <span class="text-white"><a href="#"><i class="fab fa-youtube"></i></a></span>
+                <span>Síguenos!<span>
+                {{-- Links de Redes Sociales y concurso de navidad catracha en youtube --}}
+                <span class="text-white"><a href="https://www.facebook.com/NavidadCatracha/"><i class="fab fa-facebook"></i></a></span>
+                <span class="text-white"><a href="https://www.youtube.com/watch?v=J6c7KC_a6Jg&feature=youtu.be&fbclid=IwAR31Rc0YlATa878FnE4pZ-nxmIodwajA0aHXBBfa6TvzSneS97ZP6ihm_2w"><i class="fab fa-youtube"></i></a></span>
             </div>
            {{--  <div class="col-lg-6">
                 <span class="" style="font-family:'Futura Bold'; font-size: .85rem">Paz, Unidad y Prevención</span>
@@ -13,7 +14,7 @@
         </div>
     </div>
     <div class="text-center">
-            <small class="text-white ">Todos los derechos reservados 2019 | Sub Secretaría de Seguridad y Prevención</small>
+        <small class="text-white ">Todos los derechos reservados @php echo(date('Y')) @endphp | Sub Secretaría de Seguridad y Prevención</small>
     </div>
 </div>
 
